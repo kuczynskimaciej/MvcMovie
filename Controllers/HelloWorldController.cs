cf99c4e5b50f520111e27702103c5df385ab1594
KUCZYN\kaczk <kaczksa>
2020-02-18 23:07:21 +0100

    radiobutton/checkbox

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -30,7 +30,7 @@ namespace MvcMovie.Controllers
             return View(question);
         }
         [HttpPost]
-        public IActionResult Welcome([Bind("Question")] QuestionModel question)
+        public IActionResult Welcome([Bind("Question")] QuestionModel model)
         {
             return View();
         }
--- Models/AnswerModel.cs
+++ Models/AnswerModel.cs
@@ -9,5 +9,6 @@ namespace MvcMovie.Models
     {
         public string Answer { get; set; }
         public bool IsCorrect { get; set; }
+        public bool SelectedAnswers { get; set; }
     }
 }
